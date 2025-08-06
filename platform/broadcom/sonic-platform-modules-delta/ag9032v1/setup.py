ca096e591d760c8edb9d2313bd9863a94605caa4
attackofthetitan <ethanlin.0902@gmail.com>
2025-08-07 02:49:36 +0800

    Fix wrong paths and missing modules

--- platform/broadcom/sonic-platform-modules-delta/ag9032v1/setup.py
+++ platform/broadcom/sonic-platform-modules-delta/ag9032v1/setup.py
@@ -7,6 +7,6 @@ setup(
     packages=['sonic_platform'],
     package_dir={'sonic_platform': 'ag9032v1/pddf/sonic_platform'},
     data_files=[
-        ('/usr/share/sonic/device/x86_64-delta_ag9032v1-r0/', ['ag9032v1/pddf/cfg/ag9032v1-pddf.json']),
+        ('/usr/share/sonic/device/x86_64-delta_ag9032v1-r0/', ['pddf/pddf-device.json']),
     ],
 )
